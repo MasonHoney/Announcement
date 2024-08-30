c8f93a26e31f45011ac8322cd55a0fabab65e83a
Mason <jamesmasonhoney@protonmail.com>
2024-08-29 19:44:23 -0500

    It's a girl!

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+const originalGradient = document.getElementsByTagName("h1")[0].style.backgroundImage;
 document.getElementById('Btn').addEventListener('click', function() {
     document.getElementById('initialContent').style.display = 'none';
     document.getElementById('gif-div').style.display = 'block';
+    document.getElementsByTagName("h1")[0].style.backgroundImage = "none";
+    document.getElementsByTagName("h1")[0].style.backgroundColor = "pink";
 });
 
 document.getElementById('reset').addEventListener('click', function() {
     document.getElementById('initialContent').style.display = 'block';
     document.getElementById('gif-div').style.display = 'none';
+    document.getElementsByTagName("h1")[0].style.backgroundImage = originalGradient;
+    document.getElementsByTagName("h1")[0].style.backgroundColor = "";
 });
